2dec7db66f0cc8ebae62560c89f046b420e637fd
franciowel <spina_francesco@blu.it>
2022-07-13 13:04:09 +0200

    aggiunte cover e banner lingua

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,14 +1,53 @@
 <template>
     <div class="back">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         <div class="container">
+
+            <div class="d-flex justify-content-center">
+                Lingua: 
+                <select @change="selectLang" name="lang" id="lang">
+                    <option v-for="item, index in langList" :key="index" :value="item.iso_639_1">{{item.english_name}}</option>
+                </select>
+            </div>
+
+            <h2>Film</h2>
             <ul>
                 <li class="text-center" v-for="item, index in filmList" :key="index">
-                    <h3>{{item.title}}</h3>
-                    <div>Titolo originale: {{item.original_title}}</div>
-                    <div>Lingua: {{item.original_language}}</div>
-                    <div>Voto: {{item.vote_average}}</div>
+
+                    <div class="cover">
+                        <img :src="`https://image.tmdb.org/t/p/w342/${ item.poster_path }`" :alt="item.title">
+                    </div>
+                    <!-- INFORMAZIONI DEL FILM -->
+                    <div class="hidden">
+                        <h3>{{item.title}}</h3>
+                        <div>Titolo originale: {{item.original_title}}</div>
+                        <!-- LINGUA -->
+                        <div class="language" v-if="item.original_language == 'en'">Lingua: {{item.original_language}}</div>
+                        <div v-else>
+                            <img
+                            class="flag"
+                            :src="`https://flagcdn.com/16x12/${item.original_language}.png`"
+                            :srcset="`https://flagcdn.com/32x24/${item.original_language}.png 2x,
+                                    https://flagcdn.com/48x36/${item.original_language}.png 3x`"
+                            width="20"
+                            :alt="`${item.original_language}`">
+                        </div>
+                        <!-- VALUTAZIONE -->
+                        <!-- <div>Voto: {{item.vote_average}}</div> -->
+
+                        <div class="stars">
+                            <i v-for="n in 5" :key="n" class="fa-regular fa-star" :class="{'gold': n <= voteCalc(item.vote_average) }"></i>
+                        </div>
+
+                        <div class="overview">{{item.overview}}</div>
+
+                    </div>
                 </li>
             </ul>
+
+            <h2>Serie TV</h2>
         </div>
     </div>
 </template>
@@ -21,7 +60,13 @@ export default {
     data() {
         return {
             url: 'https://api.themoviedb.org/3/search/movie?api_key=f2faa276f128d7e15803d0612da76a93&language=it-IT&query=a&page=1&include_adult=false',
+            urlTv: 'https://api.themoviedb.org/3/search/tv?page=1&query=a&include_adult=false&api_key=f2faa276f128d7e15803d0612da76a93&language=it-IT',
+            langUrl: 'https://api.themoviedb.org/3/configuration/languages?api_key=f2faa276f128d7e15803d0612da76a93',
             filmList: [],
+            tvList: [],
+            langList: [],
+            userLang: 'it',
+            userLangCapital: 'IT',
             userText: '',
         }
     },
@@ -29,15 +74,35 @@ export default {
         getFilm() {
             axios.get(this.url).then((result) =>{
                 this.filmList = result.data.results;
-            })
+            });
+            axios.get(this.urlTv).then((result) =>{
+                this.tvList = result.data.results;
+            });
         },
         searchFilm() {
             this.userText = this.text;
-            this.url = `https://api.themoviedb.org/3/search/movie?api_key=f2faa276f128d7e15803d0612da76a93&language=it-IT&query=${this.text}&page=1&include_adult=false`;
+            this.url = `https://api.themoviedb.org/3/search/movie?api_key=f2faa276f128d7e15803d0612da76a93&language=${this.userLang}-${this.userLangCapital}&query=${this.text}&page=1&include_adult=false`;
+            this.urlTv = `https://api.themoviedb.org/3/search/tv?page=1&query=${this.userText}&include_adult=false&api_key=f2faa276f128d7e15803d0612da76a93&language=${this.userLang}-${this.userLangCapital}`;
             this.filmList = [];
+            this.tvList = [];
             this.getFilm();
-        }
-    },
+        },
+        selectLang(value) {
+            this.userLang = event.target.value;
+            this.userLangCapital = this.userLang.toLocaleUpperCase();
+            console.log(value)
+            this.searchFilm();
+        },
+        getLang() {
+            axios.get(this.langUrl).then((result) =>{
+                this.langList = result.data;
+            });
+        },
+        voteCalc(mark) {
+            const count = mark / 2;
+            return Math.round(count);
+        },
+    },      
     watch: {
         text() {
             this.searchFilm();
@@ -45,7 +110,8 @@ export default {
     },
     created() {
         this.getFilm();
-    }
+        this.getLang();
+    },
 }
 </script>
 
@@ -65,5 +131,66 @@ ul {
         margin: 15px;
         width: 250px;
     }
+}
+h2 {
+    color: white;
+    font-weight: 700;
+    font-size: 35px;
+    text-align: center;
+}
+li {
+    width: 345px;
+    height: 515px;
+}
+img {
+    width: 342px;
+    max-width: 342px;
+    height: 513px;
+}
+.gold {
+    color: yellow;
+}
+.stars {
+    font-size: 20px;
+    margin: 15px;
+}
+li:hover {
+    .cover {
+        display: none;
+    }
+    .hidden {
+        display: block;
+    }
+}
+.title {
+    font-size: 20px;
+    margin: 15px;
+}
+.title:first-of-type {
+    margin-top: 40px;
+}
+
+.flag {
+    height: 20px;
+    width: 25px;
+    margin: 15px;
+}
+.lang {
+    font-size: 20px;
+    margin: 15px;
+}
+.cover {
+    display: block;
+}
+.overview {
+    margin: 15px; 
+    overflow-y: auto;
+    max-height: 400px;
+}
+.hidden {
+    display: none;
+    max-height: 515px;
+    max-width: 345px;
+    overflow-y: auto;
 }
 </style>
